c371e665d1bcb28828f209e19b24e204c5a64eed
Tollef Fog Heen <tfheen@err.no>
2011-03-29 12:06:48 +0200

    Clean up run script

--- run_gcov.sh
+++ run_gcov.sh
@@ -8,44 +8,28 @@ if [ `id -u` -eq 0 ] ; then
 fi
 
 cd $(dirname $0)
-rm -rf Html/varnish-cache/* Html/*.png
-#svn co http://varnish-cache.org/svn/trunk/varnish-cache || true
+rm -rf Html/varnish-cache/* Html/*.png varnish-cache
+git clone git://git.varnish-cache.org/varnish-cache
 (
 cd varnish-cache
-git pull
-if true ; then
-	if true; then
-		find . -name '*.gcov' -print | xargs rm -f
-		find . -name '*.gcda' -print | xargs rm -f
-		find . -name '*.gcno' -print | xargs rm -f
-
-		make clean || true
-		make distclean || true
-		export CFLAGS="-fprofile-arcs -ftest-coverage -fstack-protector" 
-		sh autogen.sh
-		T=$(mktemp -d)
-		./configure \
-			--prefix=$T \
-			--enable-debugging-symbols
-
-		make 2>&1 | tee _.make
-	fi
-
-	if true ; then
-		(
-		# Get usage() run
-		cd bin/varnishd
-		./varnishd --help > /dev/null 2>&1 || true
-		)
-		make check | tee _.test
-	fi
-
-	find -name \*.o -execdir gcov {} \;
-fi
+export CFLAGS="-fprofile-arcs -ftest-coverage -fstack-protector"
+sh autogen.sh
+T=$(mktemp -d)
+./configure \
+    --prefix=$T \
+    --enable-debugging-symbols
+make 2>&1 | tee _.make
+(
+    # Get usage() run
+    cd bin/varnishd
+    ./varnishd --help > /dev/null 2>&1 || true
+)
+make check | tee _.test
+find -name \*.o -execdir gcov {} \;
 )
 find varnish-cache -name '*.gcov' -print | ./gcov_fmt.py >> statfile.txt
 ./plot.py
 cd Html
 git add .
 git add --update .
-git commit -m"Run $(date)"
+git commit -m"Run $(date) Tree: $(cd ../varnish-cache && git describe --always)"
