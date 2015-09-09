ac2d70d5af42945ca3015623fc4b0d26619a0b76
Lasse Karstensen <lkarsten@varnish-software.com>
2015-09-09 13:34:38 +0200

    Split it up a bit.

--- run_gcov.sh
+++ run_gcov.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash -x
+#
+# apt-get install lcov
 
 set -e
 
@@ -8,30 +10,38 @@ if [ `id -u` -eq 0 ] ; then
 fi
 
 cd $(dirname $0)
-rm -rf Html/varnish-cache/* Html/*.png varnish-cache
-git clone git://git.varnish-cache.org/varnish-cache
-(
-    cd varnish-cache
-    export CFLAGS="-fprofile-arcs -ftest-coverage -fstack-protector"
-    sh autogen.sh
-    T=$(mktemp -d)
-    ./configure \
-	--prefix=$T \
-	--enable-debugging-symbols
-    make 2>&1 | tee _.make
-    lcov --directory `pwd` --zerocounters
+# rm -rf html/varnish-cache/* html/*.png varnish-cache
+if [ ! -d "varnish-cache" ]; then
+	git clone git://git.varnish-cache.org/varnish-cache
+	cd varnish-cache
+	export CFLAGS="-fprofile-arcs -ftest-coverage -fstack-protector"
+	sh autogen.sh
+	T=$(mktemp -d)
+	./configure \
+		--prefix=$T \
+		--enable-debugging-symbols
+	make 2>&1 | tee _.make
+	cd ..
+fi
+cd varnish-cache
+
+lcov --directory `pwd` --zerocounters
     (
     # Get usage() run
 	cd bin/varnishd
 	./varnishd --help > /dev/null 2>&1 || true
     )
-    make check | tee _.test
-    lcov --directory `pwd` --capture --output-file varnish.info
-)
-./lcov_fmt.py < varnish-cache/varnish.info >> Html/statfile.txt
-genhtml --output-directory=Html/varnish-cache varnish-cache/varnish.info
-./plot.py
-cd Html
-git add .
-git add --update .
-git commit -m"Run $(date) Tree: $(cd ../varnish-cache && git describe --always)"
+
+make check | tee _.test
+lcov --directory `pwd` --capture --output-file varnish.info
+cd ..
+echo "Done collecting. Formatting.."
+
+./lcov_fmt.py < varnish-cache/varnish.info >> html/statfile.txt
+genhtml --output-directory=html/varnish-cache varnish-cache/varnish.info
+./plot.py html
+#cd html
+
+#git add .
+#git add --update .
+#git commit -m"Run $(date) Tree: $(cd ../varnish-cache && git describe --always)"
